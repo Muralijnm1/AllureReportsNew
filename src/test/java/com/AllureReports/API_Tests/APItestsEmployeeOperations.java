1f2d25d9f641872dae3c72045ff51e76ac71f915
Murali Janjanam <murali.jnm@gmail.com>
2020-06-15 21:12:08 +0530

    Employee API tests with Allure implementation

--- src/test/java/com/AllureReports/API_Tests/APItestsEmployeeOperations.java
+++ src/test/java/com/AllureReports/API_Tests/APItestsEmployeeOperations.java
@@ -1,221 +1,117 @@
 package com.AllureReports.API_Tests;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.isEmptyOrNullString;
-import static org.hamcrest.Matchers.hasItems;
 import java.util.HashMap;
-
 import org.junit.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import io.restassured.response.*;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+@Listeners({ com.AllureReports.ReportsGeneration.AllureListener.class })
 public class APItestsEmployeeOperations {
 
-	
-	/*@BeforeClass
+	public static Integer id = null;
+
+	@BeforeClass
 	public void setUp() {
-		RestAssured.baseURI="http://restapi.demoqa.com/authentication/CheckForAuthentication";
-		RestAssured.authentication=RestAssured.preemptive().basic("ToolsQA", "TestPassword");
-		
-		 }*/
-	
-	@Test(enabled = false,priority=1)
-	public void postRequest_createNewEmployee() {
-		
-		 HashMap map = new HashMap();
-		 map.put("first_name", "employee14_firstname");
-		 map.put("last_name", "employee14_firstname");		 
-		 given()
-		 		.contentType(ContentType.JSON)
-		 		.body(map)
-		 .when()
-		 		.post("http://localhost:3000/employee")
-		 .then()
-		 		.statusCode(201)
-		 		.assertThat()		 		
-		 		//.root("employee")
-		 		//.body("first_name",hasItems("employee14_firstname"))
-		 		.body("first_name",equalTo("employee14_firstname"))
-		 		.body("last_name",equalTo("employee14_firstname"));
-		 }
-	@Test(enabled = false,priority=2)
-	public void getRequest_getEmployee() {
-		
-		 HashMap map = new HashMap();
-		 map.put("first_name", "employee14_firstname");
-		 map.put("last_name", "employee14_firstname");		 
-		 given()
-		 		.contentType(ContentType.JSON)
-		 		.body(map)
-		 .when()
-		 		.post("http://localhost:3000/employee/26")
-		 .then()
-		 		.statusCode(200)
-		 		.assertThat()		 		
-		 		//.root("employee")
-		 		//.body("first_name",hasItems("employee14_firstname"))
-		 		.body("first_name",equalTo("employee14_firstname"))
-		 		.body("last_name",equalTo("employee14_firstname"));
-		 }
-	@Test(enabled = false,priority=3)
-	public void putRequest_updateEmployee() {
-		
-		 HashMap map = new HashMap();
-		 map.put("first_name", "employee14_firstname");
-		 map.put("last_name", "employee14_firstname");		 
-		 given()
-		 		.contentType(ContentType.JSON)
-		 		.body(map)
-		 .when()
-		 		.post("http://localhost:3000/employee/3")
-		 .then()
-		 		.statusCode(201)
-		 		.assertThat()		 		
-		 		//.root("employee")
-		 		//.body("first_name",hasItems("employee14_firstname"))
-		 		.body("first_name",equalTo("employee14_firstname"))
-		 		.body("last_name",equalTo("employee14_firstname"));
-		 }
-	
-	@Test(enabled = false,priority=4)
-	public void patchRequest_patchEmployee() {
-		
-		 HashMap map = new HashMap();
-		 map.put("first_name", "employee14_firstname");
-		 map.put("last_name", "employee14_firstname");		 
-		 given()
-		 		.contentType(ContentType.JSON)
-		 		.body(map)
-		 .when()
-		 		.post("http://localhost:3000/employee/3")
-		 .then()
-		 		.statusCode(201)
-		 		.assertThat()		 		
-		 		//.root("employee")
-		 		//.body("first_name",hasItems("employee14_firstname"))		 		
-		 		.body("last_name",equalTo("employee14_firstname"));
-		 }
-	
-	@Test(enabled = true,priority=4)
-	public void deleteRequest_Employee() {
-		
-		 Response response = 
-		 given()
-		 			 		
-		 .when()
-		 		.delete("http://localhost:3000/employee/13")
-		 .then()
-		 		.statusCode(200)
-		 		//.assertThat()		 		
-		 		//.root("employee")
-		 		//.body("first_name",hasItems("employee14_firstname"))		 		
-		 		.extract().response();
-		 
-		 String jsonAsString=response.asString();
-		 System.out.println("The Json value "+jsonAsString);
-		 Assert.assertEquals("{}", jsonAsString);
-		 }
-	
-
-/*
-	@Test(enabled = false,priority=2)
-
-	public void getRequest() throws NoSuchFieldException, SecurityException {
-
-		System.out.println("#######  The getRequest started ###########");
-		
-		RestAssured.baseURI = "https://reqres.in";	
-
-		// Request object
-		RequestSpecification httpRequest = RestAssured.given();
-
-		// Response object
-		Response resp = httpRequest.request(Method.GET, "/api/unknown/12");
-
-		// print response in console
-
-		String responseBody = resp.getBody().asString();
-		System.out.println("The response body is " + responseBody);
-		
-		int code = resp.getStatusCode();
-		 Assert.assertEquals(200, code);
-		 Assert.assertTrue(responseBody.contains("name : honeysuckle"));
-		 
-		}
-
-	
-	@Test(enabled = false,priority=3)
-
-	public void getRequestWeather() throws NoSuchFieldException, SecurityException {
-
-		System.out.println("#######  The getRequestWeather started ###########");
-		RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
-
-		// RestAssured.baseURI="https://reqres.in";
-
-		// Request object
-		RequestSpecification httpRequest = RestAssured.given();
-
-		// Response object
-		Response resp = httpRequest.request(Method.GET, "/Hyderabad");
-
-		// print response in console
-
-		String responseBody = resp.getBody().asString();
-		System.out.println("The response body is " + responseBody);
-		
-
-		
-		 * ResponseBody<?> bodyText = resp.getBody(); //int code = 200;
-		 * 
-		 * System.out.println("Status code  "+bodyText);
-		 * //Assert.assertEquals(200, code);
-		  
-		}
-	@Test(enabled = false)
-
-	public void postRequest1() {
-
-		RestAssured.baseURI = "http://restapi.demoqa.com/customer";
-
-		// Request object
-		RequestSpecification httpRequest = RestAssured.given();
-
-		// Request payload sending along with post request
-		JSONObject requestParams = new JSONObject();
-
-		requestParams.put("FirstName", "Murali2");
-		requestParams.put("LastName", "Jan2");
-		requestParams.put("UserName", "Murali2");
-		requestParams.put("Password", "xyxYYdd2");
-		requestParams.put("Email", "xyz2@gmail.com");
-
-		httpRequest.header("Content-Type", "application/json");
-
-		httpRequest.body(requestParams.toJSONString());
-
-		// Response object
-		Response resp = httpRequest.request(Method.POST, "/register");
-
-		// print response in console
-
-		String responseBody = resp.getBody().asString();
-		System.out.println("The response body is " + responseBody);
-
-		// Status code verify
-		int statusCode = resp.getStatusCode();
-
-		System.out.println("Status code  " + statusCode);
-		// Assert.assertEquals(statusCode, 200);
-
-		// Success code verify
-		int successCode = resp.jsonPath().get("SuccessCode");
-
-		System.out.println("Status code  " + successCode);
-		// Assert.assertEquals(successCode, "OPERATION_SUCCESS");
+		RestAssured.baseURI = "http://localhost:3000";
 	}
 
-*/}
+	@Severity(SeverityLevel.BLOCKER)
+	@Test(enabled=true,priority = 1, description = "Verify empolyee creation")
+	@Description("Verify empolyee creation........")
+	@Epic("EP001")
+	@Feature("Feature1: empolyee creation")
+	@Story("Story:Empolyee creation")
+	@Step("Verify Empolyee creation")	
+	public void postRequest_createNewEmployee() {
+		RestAssured.basePath = "/employee";
+		HashMap map = new HashMap();
+		map.put("first_name", "employee14_firstname");
+		map.put("last_name", "employee14_lastname");
+
+		Response resp = given().contentType(ContentType.JSON).body(map).when().post().then().statusCode(201)
+				.assertThat().body("first_name", equalTo("employee14_firstname"))
+				.body("last_name", equalTo("employee14_lastname")).extract().response();
+		resp.prettyPrint();
+		JsonPath extractor = resp.jsonPath();
+		id = extractor.get("id");
+	}
+
+	@Severity(SeverityLevel.BLOCKER)
+	@Test(enabled=true,priority = 2, description = "Retrive empolyee")
+	@Description("Retrive empolyee creation........")
+	@Epic("EP001")
+	@Feature("Feature2: empolyee Retrive")
+	@Story("Story:Empolyee retrive")
+	@Step("Verify Empolyee retrive")	
+	public void getRequest_getEmployee() {
+		RestAssured.basePath = "employee/" + id;
+		given().when().get().then().statusCode(200).assertThat().body("first_name", equalTo("employee14_firstname"))
+				.body("last_name", equalTo("employee14_lastname"));
+	}
+
+	@Severity(SeverityLevel.BLOCKER)
+	@Test(enabled=true,priority = 3, description = "Update empolyee")
+	@Description("Update empolyee creation........")
+	@Epic("EP001")
+	@Feature("Feature3: empolyee update")
+	@Story("Story:Empolyee update")
+	@Step("Verify Empolyee update")	
+	public void putRequest_updateEmployee() {
+		RestAssured.basePath = "employee/" + id;
+		HashMap map = new HashMap();
+		map.put("first_name", "employee15_firstname");
+		map.put("last_name", "employee15_lastname");
+		given().contentType(ContentType.JSON).body(map).when().put().then().statusCode(200).assertThat()
+				.body("first_name", equalTo("employee15_firstname")).body("last_name", equalTo("employee15_lastname"));
+	}
+	
+	@Severity(SeverityLevel.BLOCKER)
+	@Test(enabled=true,priority = 4, description = "Patch empolyee")
+	@Description("Patch empolyee........")
+	@Epic("EP001")
+	@Feature("Feature4: empolyee patch")
+	@Story("Story:Empolyee patch")
+	@Step("Verify Empolyee patch")	
+	public void patchRequest_patchEmployee() {
+		RestAssured.basePath = "employee/" + id;
+		HashMap map = new HashMap();
+		map.put("first_name", "employee16_firstname");
+		given().contentType(ContentType.JSON).body(map).when().patch().then().statusCode(200).assertThat()
+				.body("first_name", equalTo("employee16_firstname")).body("last_name", equalTo("employee15_lastname"));
+	}
+
+
+	@Severity(SeverityLevel.BLOCKER)
+	@Test(enabled=true,priority = 5, description = "Delte empolyee")
+	@Description("Patch empolyee........")
+	@Epic("EP001")
+	@Feature("Feature4: empolyee delete")
+	@Story("Story:Empolyee delete")
+	@Step("Verify Empolyee delete")	
+	public void deleteRequest_Employee() {
+		RestAssured.basePath = "employee/" + id;
+		Response response = given()
+
+				.when().delete().then().statusCode(200).extract().response();
+
+		String jsonAsString = response.asString();
+		System.out.println("The Json value " + jsonAsString);
+		Assert.assertEquals("{}", jsonAsString);
+	}
+
+}
